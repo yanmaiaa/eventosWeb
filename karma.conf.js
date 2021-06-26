5aa87947ed61eca3915c611ecc364838f4599980
Yan Maia <yandamasceno01@gmail.com>
2021-06-26 13:08:23 -0300

    fix: bug de quebra nos templates

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/eventosapp'),
+      dir: require('path').join(__dirname, './coverage/module01'),
       subdir: '.',
       reporters: [
         { type: 'html' },
