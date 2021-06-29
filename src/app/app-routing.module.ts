75f93736cebd628d6fa599d37f7f7958230f10c4
Yan Maia <yandamasceno01@gmail.com>
2021-06-28 22:10:06 -0300

    guarding route activation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './errors/error404/error404.component';
 import { CreateEventComponent } from './events/create-event/create-event.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventListComponent } from './events/event-list/event-list.component';
 
 const routes: Routes = [
   { path: 'events/new', component: CreateEventComponent },
   { path: 'events', component: EventListComponent },
-  { path: 'events/:id', component: EventDetailsComponent },
+  {
+    path: 'events/:id',
+    component: EventDetailsComponent,
+    canActivate: [EventRouteActivator],
+  },
+  { path: '404', component: Error404Component },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { ToastrService } from './common/toastr.service';
 import { EventListComponent } from './events/event-list/event-list.component';
 import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator.service';
 import { EventService } from './events/shared/event.service';
 import { NavBarComponent } from './nav/nav.component';
 import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { CreateEventComponent } from './events/create-event/create-event.component';
+import { Error404Component } from './errors/error404/error404.component';
 
 @NgModule({
   declarations: [
@@ -20,13 +22,14 @@ import { CreateEventComponent } from './events/create-event/create-event.compone
     NavBarComponent,
     EventDetailsComponent,
     CreateEventComponent,
+    Error404Component,
   ],
   imports: [
     //Aqui é para importar outros módulos
     BrowserModule,
     AppRoutingModule,
   ],
-  providers: [EventService, ToastrService], //Aqui são adcionados os serviços
+  providers: [EventService, ToastrService, EventRouteActivator], //Aqui são adcionados os serviços
   bootstrap: [AppComponent],
 })
 export class AppModule {}
