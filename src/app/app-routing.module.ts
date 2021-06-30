b60db9385f7fdcbaf0b5c6893342b3669c205e0a
Yan Maia <yandamasceno01@gmail.com>
2021-06-30 20:50:36 -0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,6 +19,10 @@ const routes: Routes = [
     canActivate: [EventRouteActivator],
   },
   { path: '404', component: Error404Component },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
   { path: '', redirectTo: '/events', pathMatch: 'full' },
 ];
 
