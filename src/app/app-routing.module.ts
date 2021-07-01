8f3b75c7ed913fed3fac0552f93dbfa6f5d991ac
Yan Maia <yandamasceno01@gmail.com>
2021-06-30 21:05:29 -0300

    refacturing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Error404Component } from './errors/error404/error404.component';
-import { CreateEventComponent } from './events/create-event/create-event.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListComponent } from './events/event-list/event-list.component';
+import {
+  Error404Component,
+  CreateEventComponent,
+  EventDetailsComponent,
+  EventRouteActivator,
+  EventListComponent,
+} from './barrels';
 
 const routes: Routes = [
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ToastrService } from './common/toastr.service';
-import { EventListComponent } from './events/event-list/event-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail/event-thumbnail.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventService } from './events/shared/event.service';
-import { NavBarComponent } from './nav/nav.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event/create-event.component';
-import { Error404Component } from './errors/error404/error404.component';
+import {
+  ToastrService,
+  EventListComponent,
+  EventThumbnailComponent,
+  EventRouteActivator,
+  EventService,
+  NavBarComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  Error404Component,
+  AppComponent,
+  AppRoutingModule,
+} from './barrels';
 
 @NgModule({
   declarations: [
